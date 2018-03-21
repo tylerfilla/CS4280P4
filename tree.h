1719789eb95d74deae4e9731efd766a79b8139c2
Tyler Filla <tsfn88@delmar.umsl.edu>
2018-03-21 17:25:54 -0500

    add tree.h

---
+++ tree.h
@@ -0,0 +1,23 @@
+/*
+ * Tyler Filla
+ * CS 4280
+ * Project 2
+ */
+
+#ifndef P2_TREE_H
+#define P2_TREE_H
+
+namespace p2
+{
+
+class binary_op
+{
+};
+
+class unary_op
+{
+};
+
+} // namespace p2
+
+#endif // #ifndef P2_TREE_H
